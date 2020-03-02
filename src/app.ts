279bd0688517837f8d72da1752286458d3356754
pipopotamasu <ec0156hx@gmail.com>
2020-03-02 19:00:35 +0900

    rm middleware

--- src/app.ts
+++ src/app.ts
@@ -2,7 +2,6 @@ import express from "express";
 import session from "express-session";
 import bodyParser from "body-parser";
 import mongo from "connect-mongo";
-import path from "path";
 import mongoose from "mongoose";
 import bluebird from "bluebird";
 import { MONGODB_URI, SESSION_SECRET } from "./util/secrets";
@@ -39,29 +38,6 @@ app.use(session({
         autoReconnect: true
     })
 }));
-app.use((req, res, next) => {
-    res.locals.user = req.user;
-    next();
-});
-app.use((req, res, next) => {
-    // After successful login, redirect back to the intended page
-    if (!req.user &&
-    req.path !== "/login" &&
-    req.path !== "/signup" &&
-    !req.path.match(/^\/auth/) &&
-    !req.path.match(/\./)) {
-        req.session.returnTo = req.path;
-    } else if (req.user &&
-    req.path == "/account") {
-        req.session.returnTo = req.path;
-    }
-    next();
-});
-
-app.use(
-    express.static(path.join(__dirname, "public"), { maxAge: 31557600000 })
-);
-
 /**
  * API examples routes.
  */
