b282431cca73298c909e9eb325a1306ab32ac7c9
pipopotamasu <ec0156hx@gmail.com>
2020-03-19 22:27:07 +0900

    rm console

--- src/controllers/v1/posts.ts
+++ src/controllers/v1/posts.ts
@@ -5,7 +5,6 @@ import { Post } from "@models/Post";
 
 export const getPosts = (req: Request, res: Response) => {
     Post.find((err, result) => {
-        console.log(11111, result)
         res.json(result);
     });
 };
