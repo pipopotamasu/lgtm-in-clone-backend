98df5ec598332972af6960e952d45051bac04064
pipopotamasu <ec0156hx@gmail.com>
2020-03-20 16:38:58 +0900

    fix indent rule

--- test/api.test.ts
+++ test/api.test.ts
@@ -2,8 +2,8 @@ import request from "supertest";
 import app from "../src/app";
 
 describe("GET /api", () => {
-    it("should return 200 OK", () => {
-        return request(app).get("/api/v1")
-            .expect(200);
-    });
+  it("should return 200 OK", () => {
+    return request(app).get("/api/v1")
+      .expect(200);
+  });
 });
