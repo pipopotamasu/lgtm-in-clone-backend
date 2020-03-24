798f9c1ddaeb0d600213ee7cf367ab2b306035ca
pipopotamasu <ec0156hx@gmail.com>
2020-03-25 08:37:57 +0900

    mongo connect only once

---
+++ test/jest.setup.global.ts
@@ -0,0 +1,11 @@
+import mongoose from "mongoose";
+
+module.exports = async () => {
+  mongoose.Promise = global.Promise;
+
+  mongoose.connection
+    .once("open", () => { console.log("connected mongodb"); })
+    .on("error", (error) => {
+      console.warn("Warning", error);
+    });
+}
