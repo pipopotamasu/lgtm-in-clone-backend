ab260ffec69cfa3f1270a70db67f3188c687e5d3
pipopotamasu <ec0156hx@gmail.com>
2020-03-24 21:50:45 +0900

    login

--- src/controllers/v1/users.ts
+++ src/controllers/v1/users.ts
@@ -1,6 +1,36 @@
-import { User } from "@models/User";
+import { User, UserDocument } from "@models/User";
 import { Request, Response, NextFunction } from "express";
 import { check, validationResult } from "express-validator";
+import passport from "passport";
+import { IVerifyOptions } from "passport-local";
+
+/**
+ * POST /login
+ * Sign in using email and password.
+ */
+export const postLogin = async (req: Request, res: Response, next: NextFunction) => {
+  await check("email", "Email is not valid").isEmail().normalizeEmail().run(req);
+  await check("password", "Password must be at least 4 characters long").isLength({ min: 4 }).run(req);
+
+	const errors = validationResult(req);
+
+  if (!errors.isEmpty()) {
+    return res.status(400).json(errors);
+  }
+
+	passport.authenticate("local", (err: Error, user: UserDocument) => {
+		if (err) { return res.status(500).json({ errors: [err] }); }
+		if (!user) {
+			return res.status(401).json({ errors: [{ msg: "invalid account" }] });
+		}
+			req.logIn(user, (err) => {
+				if (err) {
+					return res.status(500).json({ errors: [err] });
+				}
+				return res.status(200).json({ user });
+			});
+	})(req, res);
+};
 
 /**
  * POST /signup
