d2e852219210633de5b7bc2e33410b0dc78c8185
pipopotamasu <ec0156hx@gmail.com>
2020-04-13 00:57:04 +0900

    use create

--- src/controllers/v1/posts.ts
+++ src/controllers/v1/posts.ts
@@ -13,16 +13,14 @@ export const createPost = async (req: Request, res: Response) => {
   upload(req, res, async (err) => {
     if (err) {
       return res.status(500).json({
-        error: "fail to uplord image"
+        errors: ["fail to upload image"]
       })
     }
 
-    const post = new Post({
+    const post = await Post.create({
       userId: (req.user as any).id,
       src: res.req.file.filename
     })
-
-    await post.save();
 
     return res.status(201).json({
       post: post.response()
