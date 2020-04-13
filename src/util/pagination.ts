17ca642254ad6c552e4b0d3cfb97542195e23849
pipopotamasu <ec0156hx@gmail.com>
2020-04-13 16:43:47 +0900

    refactor

--- src/util/pagination.ts
+++ src/util/pagination.ts
@@ -1,5 +1,5 @@
 export function calcPagination (pageQuery: string | undefined, limit: number) {
-  const page = parseInt(pageQuery ? pageQuery : "");
+  const page = parseInt(pageQuery || "");
   if (isNaN(page) || page <= 0) {
     return 0;
   }
