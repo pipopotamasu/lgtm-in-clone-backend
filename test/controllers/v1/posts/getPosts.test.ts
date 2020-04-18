867e4b1b8e88ae187594c2fed1976bc63050f2bf
pipopotamasu <ec0156hx@gmail.com>
2020-04-19 00:42:53 +0900

    get posts with bookmark status (#59)

--- test/controllers/v1/posts/getPosts.test.ts
+++ test/controllers/v1/posts/getPosts.test.ts
@@ -1,6 +1,8 @@
 import request from "supertest";
 import app from "@src/app";
 import { Post } from "@models/Post";
+import { PostBookmark } from "@models/PostBookmark";
+import { login } from "@test/helpers/user";
 
 describe("Get /api/v1/posts", () => {
   it("is successfull get posts as desc order", async () => {
@@ -18,5 +20,15 @@ describe("Get /api/v1/posts", () => {
     }
     const res = await request(app).get("/api/v1/posts?page=2").expect(200);
     expect(res.body.posts[0].userId).toBe("testuserid1");
+  });
+
+  it("is successfull get posts with bookmark status", async () => {
+    const { loginCookie, user } = await login();
+    const post = new Post({ src: "path/to/src", userId: "testuserid" });
+    const bookmark = await PostBookmark.create({ postId: post.id, userId: user.id });
+    post.bookmarks.push(bookmark);
+    await post.save();
+    const res = await request(app).get("/api/v1/posts").set("Cookie", [loginCookie]).expect(200);
+    expect(res.body.posts[0].bookmarked).toBe(true);
   });
 });
