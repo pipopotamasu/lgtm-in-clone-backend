edc4c9068a6620c22824565574b9b4afc7fcf519
pipopotamasu <ec0156hx@gmail.com>
2020-05-16 15:59:34 +0900

    fix login

--- src/controllers/v1/users.ts
+++ src/controllers/v1/users.ts
@@ -30,7 +30,7 @@ export const login = async (req: Request, res: Response) => {
       if (err) {
         return res.status(500).json({ errors: [err] });
       }
-      return res.status(200).json({ user: user.response() });
+      return res.status(200).json(user.response());
     });
   })(req, res);
 };
@@ -124,7 +124,6 @@ export const signupWithMailActivation = async (req: Request, res: Response) => {
 
     createTransporter().sendMail(message, (err) => {
       if (err) {
-        console.log(err);
         return res.status(500).json({ errors: [err] });
       }
 
